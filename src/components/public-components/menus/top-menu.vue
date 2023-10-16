a5a156c0f82cbc67a884f6f10becf9320e35092b
rabbit-lili <205500758@qq.com>
2023-10-16 19:12:41 +0800

    [feat]新增登录和布局草稿及修复规范

--- src/components/public-components/menus/top-menu.vue
+++ src/components/public-components/menus/top-menu.vue
@@ -1,3 +1,12 @@
+<!--
+ * @Author: 李立
+ * @Date: 2023-10-16 18:45:02
+ * @LastEditors: 李立
+ * @LastEditTime: 2023-10-16 19:05:32
+ * @FilePath: \apiObjectc:\Users\Farben\Desktop\allObj\pc-vue3-ts\src\components\public-components\menus\top-menu.vue
+ * @Description:  
+ * 相关开发文件头自动添加：联系微信huaiplayboy
+-->
 <template>
   <div class="top-menu">
     <div class="c-red font-20rpx">top-Menu</div>

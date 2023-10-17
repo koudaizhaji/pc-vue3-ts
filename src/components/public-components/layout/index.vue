89f179ccc518f6627c001e4dca81ed6b5af20867
telescope114 <guoruifeng@bjuci.com.cn>
2023-10-17 15:46:09 +0800

    feat: modify directories

---
+++ src/components/public-components/layout/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import TopMenu from './top-menu.vue'
+import Header from './header.vue'
+import MainMenu from './main-menu.vue'
+</script>
+
+<template>
+  <el-container class="main-content">
+    <el-aside width="50px">
+      <TopMenu />
+    </el-aside>
+    <el-aside width="150px">
+      <MainMenu />
+    </el-aside>
+    <el-container>
+      <el-header height="30px"><Header /></el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+      <el-footer height="10px">Footer</el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped lang="less">
+.el-aside {
+  background-color: palegoldenrod;
+}
+.el-header {
+  // background-color: skyblue;
+  background-color: skyblue;
+}
+.el-main {
+  background-color: salmon;
+}
+</style>
--- src/views/base/home/home.vue
+++ src/views/base/home/home.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="main">
-    <el-container class="main-content">
-      <el-aside width="50px">
-        <TopMenu />
-      </el-aside>
-      <el-aside width="150px">
-        <MainMenu />
-      </el-aside>
-      <el-container>
-        <el-header height="30px"><Header /></el-header>
-        <el-main>Main</el-main>
-        <el-footer height="10px">Footer</el-footer>
-      </el-container>
-    </el-container>
+    <Layout />
   </div>
 </template>
 
 <script setup lang="ts">
-import TopMenu from '@/components/public-components/menus/top-menu.vue'
-import MainMenu from '@/components/public-components/menus/main-menu.vue'
-import Header from '@/components/public-components/menus/header.vue'
+import Layout from '@/components/public-components/layout/index.vue'
 </script>
 
 <style lang="less" scoped>
@@ -31,15 +17,5 @@ import Header from '@/components/public-components/menus/header.vue'
 .main-content {
   height: 100%;
 
-  .el-aside {
-    background-color: palegoldenrod;
-  }
-  .el-header {
-    // background-color: skyblue;
-    background-color: skyblue;
-  }
-  .el-main {
-    background-color: salmon;
-  }
 }
 </style>
